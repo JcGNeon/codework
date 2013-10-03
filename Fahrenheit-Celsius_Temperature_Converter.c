6624831e2cc251343d0dabf11860d6e42881b6cf
JcGNeon <hisame129375860@hotmail.com>
2013-10-03 16:06:10 -0400

    Update Fahrenheit-Celsius_Temperature_Converter.c
    Included lowercase letters in the 'if' statements and fixed a bug with the conversion formulas.

--- Fahrenheit-Celsius_Temperature_Converter.c
+++ Fahrenheit-Celsius_Temperature_Converter.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-#define FREEZE_PT 32.00f
-#define CONVERSION_F (5.00/9.00f)
-#define CONVERSION_C (9.00/5.00f)
+#define FREEZE_PT 32.0f
+#define SCALE_FACTOR_F (5.0/9.0f)
+#define SCALE_FACTOR_C (9.0/5.0f)
 
 void Celsius(float fahrenheit)
 {
-	printf("%f.\n", CONVERSION_C * (fahrenheit - FREEZE_PT)); // convert from fahrenheit to celsius
+	printf("%f.\n", SCALE_FACTOR_F * (fahrenheit - FREEZE_PT)); // convert from fahrenheit to celsius
 }
 
 void Fahrenheit(float celsius)
 {
-	printf("%f.\n", (CONVERSION_F * celsius) + FREEZE_PT); // convert from celsius to fahrenheit
+	printf("%f.\n", (SCALE_FACTOR_C * celsius) + FREEZE_PT); // convert from celsius to fahrenheit
 }
 
 int main(int argc, char *argv[])
@@ -19,16 +19,16 @@ int main(int argc, char *argv[])
 	char choice;
 	float fahrenheit;
 	float celsius;
-	
+
 	printf("Do you want to convert to Fahrenheit or to Celsius? (F/C)\n");
 	scanf("%c", &choice);
-	
-	if(choice == 'C') {
+
+	if((choice == 'C') || (choice == 'c')) {
 		printf("Enter the temperature in Fahrenheit that you wish to convert to Celsius: ");
 		scanf("%f", &fahrenheit);
 		Celsius(fahrenheit);
 	}
-	else if(choice == 'F') {
+	else if((choice == 'F') || (choice == 'f')) {
 		printf("Enter the temperature in Celsius that you wish to convert to Fahrenheit: ");
 		scanf("%f", &celsius);
 		Fahrenheit(celsius);
